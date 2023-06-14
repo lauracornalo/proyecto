47df00009ebfdeb394ebc287efc67ca800754880
laura cornalo <lauracornalo@hotmail.com>
2023-06-14 19:56:02 -0300

    cambie nombre de app

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world5!"
+    return "Hello world!"
 
 if __name__ ==  "__main__":
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from app import index
 
 def test_index():
-    assert index() == "Hello world5!"
+    assert index() == "Hello world!"
 
